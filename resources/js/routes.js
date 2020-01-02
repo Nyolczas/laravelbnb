c755baaec2ddee15f7d902f86601f3866d966fb3
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2020-01-02 16:00:47 +0100

    vue routes

---
+++ resources/js/routes.js
@@ -0,0 +1,17 @@
+import VueRouter from 'vue-router';
+import ExampleComponent from './components/ExampleComponent.vue';
+
+const routes = [
+    {
+        path: "/",
+        component: ExampleComponent,
+        name: "home",
+    },
+];
+
+const router = new VueRouter({
+    routes,
+    mode: "history",
+});
+
+export default router;
