e7714cc9e208c65e8dc3fe88fcbf365c183d79e8
Nyolczas <nyolczas@gmail.com>
2020-01-03 16:51:53 +0100

    Bookables

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -1,24 +1,32 @@
 <template>
-    <div>
-        <nav class="navbar navbar-expand-sm navbar-light bg-light">
-            <router-link class="navbar-brand" v-bind:to="{name: 'Főoldal'}">Laravel BNB</router-link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+  <div>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+      <router-link class="navbar-brand" v-bind:to="{ name: 'Főoldal' }">Laravel BNB</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarColor03"
+        aria-controls="navbarColor03"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-            <div class="collapse navbar-collapse" id="navbarColor03">
-                <ul class="navbar-nav mr-auto">
-                <!-- <li class="nav-item active">
+      <div class="collapse navbar-collapse" id="navbarColor03">
+        <ul class="navbar-nav mr-auto">
+          <!-- <li class="nav-item active">
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li> -->
-                <li class="nav-item">
-                    <router-link class="nav-link" v-bind:to="{name: 'Második oldal'}">Második oldal</router-link>
-                </li>
-                </ul>
-            </div>
-        </nav>
-        <div class="container my-4 px-4">
-            <router-view></router-view>
-        </div>
+          </li>-->
+          <li class="nav-item">
+            <router-link class="nav-link" v-bind:to="{ name: 'Második oldal' }">Második oldal</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container my-4 px-4">
+      <router-view></router-view>
     </div>
+  </div>
 </template>
---
+++ resources/js/bookables/BookableListItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card mb-4">
+    <div class="card-body">
+      <h5 class="card-title">{{ itemTitle }}</h5>
+      <p class="card-text">{{ itemContent }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { itemTitle: String, itemContent: String, price: Number }
+}
+</script>
+
+<style scoped>
+    .card {
+        box-shadow : 0 0 12px rgba(0, 0, 0, .2);
+    }
+</style>
